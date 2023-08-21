6162ae514921e8606393a33292310bfc44cd8db2
Brian S Paskin <21156194+bpaskin@users.noreply.github.com>
2023-08-21 11:37:03 -0400

    added more VH for containers

--- appConfig.py
+++ appConfig.py
@@ -4,6 +4,10 @@ Cell=AdminConfig.getid('/Cell:' + AdminControl.getCell() + '/')
 NodeName=AdminControl.getNode()
 
 VH=AdminConfig.getid('/VirtualHost:default_host/')
+AdminConfig.create('HostAlias', VH, '[[hostname "*"][port "9044"]]')
 AdminConfig.create('HostAlias', VH, '[[hostname "*"][port "19443"]]')
+AdminConfig.create('HostAlias', VH, '[[hostname "*"][port "19444"]]')
+AdminConfig.create('HostAlias', VH, '[[hostname "*"][port "19080"]]')
+AdminConfig.create('HostAlias', VH, '[[hostname "*"][port "19081"]]')
 
 AdminConfig.save()
